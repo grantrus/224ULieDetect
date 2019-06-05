38a5393327054e93581846063fa150deb294f426
mauriw <mauriw@stanford.edu>
2019-06-05 00:14:04 -0700

    update subject modules

--- subject.py
+++ subject.py
@@ -5,6 +5,7 @@ outward facing functions:
 get_subject_train()
 get_subject_valid()
 get_subject_test()
+get_classes()
 """
 
 import numpy as np
@@ -16,11 +17,11 @@ train = get_train(6)
 valid = get_valid(6)
 test = get_test(6)
 
-#important methods
-def get_classes(col):
+#internal methods
+def _get_classes(col):
     return list({i.strip().lower() for s in col for i in s.split(',')})
 
-def get_list(col):
+def _get_list(col):
     total = [[] for i in range(len(col))]
     for idx, s in enumerate(col):
         if type(s) is float: #NaN
@@ -30,18 +31,21 @@ def get_list(col):
     return total
 
 #initialize by fitting the encoder to train)
-classes = list(get_classes(train.subject))
-enc = MultiLabelBinarizer(classes=CLASSES)
-processed_train = get_list(train.subject)
+classes = list(_get_classes(train.subject))
+enc = MultiLabelBinarizer(classes=classes)
+processed_train = _get_list(train.subject)
 enc.fit(processed_train)
+
+def get_classes():
+    return classes
 
 def get_subject_train():
     return enc.transform(processed_train)
 
 def get_subject_valid():
-    return enc.transform(get_list(valid.subject))
+    return enc.transform(_get_list(valid.subject))
 
 def get_subject_test():
-    return enc.transform(get_list(test.subject))
+    return enc.transform(_get_list(test.subject))
 
 

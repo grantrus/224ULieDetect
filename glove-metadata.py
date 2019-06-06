3c85e1ec8dd7fa29b0b90e276c5afb10e8a323a0
mauriw <mauriw@stanford.edu>
2019-06-06 00:05:10 -0700

    making progress on metadata

---
+++ glove-metadata.py
@@ -0,0 +1,70 @@
+"""
+Gets sentence-level embeddings for the speaker's jobs and venue by averaging word-by-word GloveEmbeddings.
+
+outward facing functions:
+get_glove()
+
+returns ndarray of shape is (num sentences, embedding size)
+
+Can modify size of the glove embedding size. Options are {50, 100, 200, 300}. Default is 50
+
+---
+Options are "speaker's job" and "venue"
+
+TODO: experiment with tf-idf weighting of glove embeddings
+"""
+
+from collections import Counter
+import numpy as np
+from nltk.tokenize import word_tokenize as tokenize
+
+#get data
+from setup import get_train, get_valid, get_test
+train = get_train(6)
+valid = get_valid(6)
+test = get_test(6)
+
+GLOVE_SHAPE = 50 #can be changed
+
+#import glove from parent directory
+import os, sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import utils
+GLOVE_HOME = os.path.join('..','data', 'glove.6B')
+glove = utils.glove2dict(os.path.join(GLOVE_HOME, 'glove.6B.' + str(GLOVE_SHAPE) + 'd.txt'))
+
+def get_most_common(col):
+    job = col
+    total = []
+    for j in job:
+        if type(j) is not float:
+            total.append(j.strip())
+    c = Counter(total)
+    most = c.most_common()
+    return most
+
+def get_embeddings(col):
+    total = []
+    for idx, s in enumerate(col):
+        avg = np.zeros((1, GLOVE_SHAPE)) #initialize avg
+
+        if type(s) is not float:
+            tokenized = tokenize(s)
+            for t in tokenized:
+                t = t.lower()
+                if t in glove: #ignore if not in glove dictionary
+                    emb = glove.get(t).reshape(1, -1)
+                    avg += emb
+            if not len(tokenized):
+                assert False, 'job at index {} is not valid'.format(idx)
+            avg /= len(tokenized)
+
+        total.append(avg) #if NaN (no job listed), embedding is a 0 array
+
+    return np.concatenate(total, axis = 0)
+
+def get_glove(col):
+    #options are "speaker's job" and "venue"
+    return (get_embeddings(train[col]), 
+            get_embeddings(valid[col]), 
+            get_embeddings(test[col]))
---
+++ one-hot-metadata.py
@@ -0,0 +1,53 @@
+"""
+returns a multiclass one hot encoding for the subject's of train, valid, and test
+
+
+outward facing functions:
+get_one_hot()
+
+options:
+"state info" "subject"
+
+"""
+
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
+
+from setup import get_train, get_valid, get_test
+
+train = get_train(6)
+valid = get_valid(6)
+test = get_test(6)
+
+#internal methods
+def _get_classes(col):
+    return list({i.strip().lower() for s in col if type(s) is not float for i in s.split(',') })
+
+def _get_list(col):
+    total = [[] for i in range(len(col))]
+    for idx, s in enumerate(col):
+        if type(s) is float: #NaN
+            continue
+        features = [i.strip().lower() for i in s.split(',')]
+        total[idx] = features
+    return total
+
+def get_one_hot(col):
+    classes = _get_classes(train[col])
+    processed_train = _get_list(train[col])
+    enc = MultiLabelBinarizer(classes=classes)
+
+    out_train = enc.fit_transform(processed_train)
+    out_valid = enc.transform(_get_list(valid[col]))
+    out_test  = enc.transform(_get_list(test[col]))
+    
+    return out_train, out_valid, out_test
+
+
+if __name__ == "__main__":
+    out_train, out_valid, out_test = get_encodings('state info')
+    print("subjects one hot encoding's shape: " + str(out_train.shape))
+#     out_train, out_valid, out_test = get_encodings('venue')
+#     print("venue's train one hot encoding's shape: " + str(out_train.shape))
+
+

cb1ec55f0d4b22f00f012da1503907a5de719f84
Nicholas Barbier <nbarb@stanford.edu>
2019-06-06 00:34:39 -0700

    added deep neural netowkr and updated BERT.ipynb

--- torch_deep_neural_classifier.py
+++ torch_deep_neural_classifier.py
@@ -1,8 +1,14 @@
+from torch_shallow_neural_classifier import TorchShallowNeuralClassifier
 import torch.nn as nn
 
 class TorchDeepNeuralClassifier(TorchShallowNeuralClassifier):
-    def __init__(self, dropout_prob=0.7, **kwargs):
+    def __init__(self, dropout_prob=0.0, **kwargs):
         self.dropout_prob = dropout_prob
         super().__init__(**kwargs)
     
     def define_graph(self):
+    	return nn.Sequential(
+            nn.Linear(self.input_dim, self.hidden_dim),
+            nn.Dropout(p = self.dropout_prob),
+            self.hidden_activation,
+            nn.Linear(self.hidden_dim, self.n_classes_))
